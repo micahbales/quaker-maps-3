ab8c5a1012105b480c76469f2092a3c511835582
Micah Bales <micahbales@gmail.com>
2018-09-06 23:09:49 -0400

    Create Beginning Migration and Additional Seed Data

--- api/migrations/sqls/20180907002504-initial-migration-down.sql
+++ api/migrations/sqls/20180907002504-initial-migration-down.sql
@@ -1,1 +1,8 @@
-/* Replace with your SQL commands */
+DROP TABLE meeting;
+DROP TABLE meeting_yearly_meeting;
+DROP TABLE branch;
+DROP TABLE meeting_branch;
+DROP TABLE worship_style;
+DROP TABLE meeting_worship_style;
+DROP TABLE accessibility;
+DROP TABLE meeting_accessibility;
--- api/migrations/sqls/20180907002504-initial-migration-up.sql
+++ api/migrations/sqls/20180907002504-initial-migration-up.sql
@@ -1,11 +1,80 @@
--- Create Meetings
+-- Create Meeting
+CREATE TABLE meeting (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    mappable BOOLEAN,
+    phone VARCHAR(255),
+    email VARCHAR(255),
+    city VARCHAR(255),
+    address VARCHAR(255),
+    zip VARCHAR(255),
+    latitude SMALLINT,
+    longitude SMALLINT,
+    description VARCHAR(255),
+    worship_time TIME,
+    state VARCHAR(255),
+    website VARCHAR(255),
+    lgbt_affirming BOOLEAN,
+    created TIMESTAMP default current_timestamp,
+    updated TIMESTAMP default current_timestamp
+);
 
--- Create MeetingYearlyMeetings
+-- Create MeetingYearlyMeeting
+CREATE TABLE meeting_yearly_meeting (
+    id SERIAL PRIMARY KEY,
+    meeting_id SMALLINT,
+    yearly_meeting_id SMALLINT,
+    created TIMESTAMP default current_timestamp,
+    updated TIMESTAMP default current_timestamp
+);
 
--- Create Branches
+-- Create Branch
+CREATE TABLE branch (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    created TIMESTAMP default current_timestamp,
+    updated TIMESTAMP default current_timestamp
+);
 
--- Create MeetingBranches
+-- Create MeetingBranch
+CREATE TABLE meeting_branch (
+    id SERIAL PRIMARY KEY,
+    meeting_id SMALLINT,
+    branch_id SMALLINT,
+    created TIMESTAMP default current_timestamp,
+    updated TIMESTAMP default current_timestamp
+);
 
--- Create Worship Styles
+-- Create Worship Style
+CREATE TABLE worship_style (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    created TIMESTAMP default current_timestamp,
+    updated TIMESTAMP default current_timestamp
+);
 
--- Create MeetingWorshipStyles
+-- Create MeetingWorshipStyle
+CREATE TABLE meeting_worship_style (
+    id SERIAL PRIMARY KEY,
+    meeting_id SMALLINT,
+    worship_style_id SMALLINT,
+    created TIMESTAMP default current_timestamp,
+    updated TIMESTAMP default current_timestamp
+);
+
+-- Create Accessibility
+CREATE TABLE accessibility (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    created TIMESTAMP default current_timestamp,
+    updated TIMESTAMP default current_timestamp
+);
+
+-- Create MeetingAccessibility
+CREATE TABLE meeting_accessibility (
+    id SERIAL PRIMARY KEY,
+    meeting_id SMALLINT,
+    accessibility_id SMALLINT,
+    created TIMESTAMP default current_timestamp,
+    updated TIMESTAMP default current_timestamp
+);
