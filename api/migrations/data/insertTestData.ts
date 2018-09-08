5da7e2c5565115448441d33d8ce6e0cfa7126e38
Micah Bales <micahbales@gmail.com>
2018-09-08 15:24:44 -0400

    Seed Sample MeetingYearlyMeeting Data & Streamline Insert Script

--- api/migrations/data/insertTestData.ts
+++ api/migrations/data/insertTestData.ts
@@ -2,6 +2,7 @@ import dotenv = require ('dotenv');
 dotenv.config();
 import {Pool, Client} from 'pg';
 import sampleMeetings = require('./sampleMeetings.json');
+import sampleMeetingYearlyMeetings = require('./sampleMeetingYearlyMeetings');
 const pool = new Pool();
 
 async function insertTestData(records) {
@@ -12,16 +13,16 @@ async function insertTestData(records) {
 
         // Get keys and values for the record to be inserted
         const recordKeys: string = Object.keys(record).join(',');
-        let recordValues: string = Object.values(record).map((value) => `\'${value}\'`).join(',');
-        // Handle for some of our values being numbers
-        recordValues = recordValues.split(',').map((value: string) => {
+        const recordValues: string = Object.values(record).map((value) => `\'${value}\'`)
+        .map((value: string) => {
             const strippedValue: string | number = value.replace(/'/g, '');
             return isNaN(strippedValue) ? value : Number(strippedValue);
         }).join(',');
 
-        console.log(`INSERT INTO ${tableType} (${recordKeys}) VALUES(${recordValues});`);
+        const queryString = `INSERT INTO ${tableType} (${recordKeys}) VALUES(${recordValues});`;
 
-        await pool.query(`INSERT INTO ${tableType} (${recordKeys}) VALUES(${recordValues});`)
+        console.log(queryString);
+        await pool.query(queryString)
         .catch((err) => { console.error('query error: ', err); });
     }
 
@@ -29,4 +30,5 @@ async function insertTestData(records) {
 }
 
 insertTestData(sampleMeetings);
+insertTestData(sampleMeetingYearlyMeetings);
 
