fa03cfb0325635b7acda9e2b0da3b1f141dcb051
Micah Bales <micahbales@gmail.com>
2018-09-21 21:56:06 -0400

    Proxy API to React Client and Display Stringified Meeting Data
    We're wanting to use this data to create maps, but first let's make sure we can access it in the client.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,16 +4,39 @@ import './App.css';
 import logo from './logo.svg';
 
 class App extends React.Component {
+
+  public state = {
+    meetings: {}
+  };
+
+  public componentDidMount() {
+    this.callApi()
+          .then(res => this.setState({ meetings: res.meetings }))
+          .catch(err => console.log(err));
+  }
+
+  public callApi = async () => {
+    const response = await fetch('/meetings');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Quaker Maps</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <h3>Meetings:</h3>
+            {
+              [this.state].map((meeting, i) => {
+                return <p key={i}>{JSON.stringify(meeting)}</p>
+              })
+            }
       </div>
     );
   }
