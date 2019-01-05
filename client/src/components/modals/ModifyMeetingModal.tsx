806154cb965c34c1bc846132c049bf538853065f
Micah Bales <micahbales@gmail.com>
2019-01-04 23:59:59 -0800

    Add Multi-Select for Yearly Meeting
    To update a meeting's yearly meetings, we'll need to be able to select more than one.

--- client/src/components/modals/ModifyMeetingModal.tsx
+++ client/src/components/modals/ModifyMeetingModal.tsx
@@ -5,10 +5,14 @@ import {Meeting, ModifyMeetingModalProps} from '../../Definitions';
 class ModifyMeetingModal extends React.Component<ModifyMeetingModalProps> {
 
     public meeting: Meeting;
+    public yearlymeetings = ['Great Plains Yearly Meeting', 'EFC - Mid America Yearly Meeting'];
+    public state: any;
 
     constructor(props: ModifyMeetingModalProps) {
         super(props);
-        this.meeting = this.props.meeting;
+        this.state = {
+            meeting: this.props.meeting,
+        };
         this.handleDeleteMeeting = this.handleDeleteMeeting.bind(this);
     }
 
@@ -18,13 +22,18 @@ class ModifyMeetingModal extends React.Component<ModifyMeetingModalProps> {
     }
 
     public handleDeleteMeeting() {
-        fetch(`/api/v1/meetings/${this.meeting.id}`, {
+        fetch(`/api/v1/meetings/${this.state.meeting.id}`, {
             method: 'DELETE',
         })
         .then(() => {
             this.props.history.push('/');
             this.props.history.go();
         });
+    }
+
+    public handleInputChange(criterion: string) {
+        console.log(criterion);
+        // console.log(this.state.meeting.yearly_meeting.map((ym: Meeting) => ym.title));
     }
 
     public render() {
@@ -34,7 +43,21 @@ class ModifyMeetingModal extends React.Component<ModifyMeetingModalProps> {
                     <span className='close' onClick={this.handleModalClose}>
                         &times;
                     </span>
-                    <h2>Update {this.meeting.title}</h2>
+                    <h2>Update {this.state.meeting.title}</h2>
+
+                    <select className='yearlymeeting' 
+                            multiple={true}
+                            value={this.state.meeting.yearly_meeting.map((ym: Meeting) => ym.title)}
+                            onChange={this.handleInputChange.bind(this, 'yearly_meeting')}>
+                            <option value=''>Yearly Meeting</option>
+                            {
+                                this.yearlymeetings.map((s: string, i) => {
+                                    return (
+                                        <option value={s} key={i}>{s}</option>
+                                    );
+                                })
+                            }
+                        </select>
 
                     <button onClick={this.handleDeleteMeeting}>Delete Meeting</button>
                 </div>
